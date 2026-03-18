de5605a1e86b295e48aa294fd90949273799d0f0
PedroH. <109111494+pedrouxz@users.noreply.github.com>
2026-03-18 19:08:54 -0300

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ tempo_medio = float(input("Digite o tempo médio de uso diário em horas. "))
 consumo_Mensal = (potencia_aparelho * tempo_medio * 30) / 1000
 custo_Estimado = consumo_Mensal * 0.5
 
+
 # Saída de dados.
 print(f"Aparelho: {nome_aparelho}")
 print(f"Consumo estimado: {consumo_Mensal} kWh/ mês.")
-print(f"Custo estímado: R${custo_Estimado:.2f} por kWh.")
+print(f"Custo estímado: R${custo_Estimado:.2f} por mês.")
